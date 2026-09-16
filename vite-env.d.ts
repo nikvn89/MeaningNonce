50ef4a8ace59e5b1f4cd2808e2ba271098ae0c35
nikvn89 <nikkyvn89@gmail.com>
2026-09-16 08:19:22 +0700

    Add files via upload

---
+++ vite-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CONTRACT_ADDRESS?: string;
+  readonly VITE_GENLAYER_RPC_URL?: string;
+  readonly VITE_GENLAYER_CHAIN_ID?: string;
+  readonly VITE_GENLAYER_CHAIN_NAME?: string;
+  readonly VITE_GENLAYER_SYMBOL?: string;
+  readonly VITE_EXPLORER_URL?: string;
+  readonly VITE_RUNTIME_CASE_ID?: string;
+  readonly VITE_ROLE_CASE_ID?: string;
+  readonly VITE_DEMO_VIDEO_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
